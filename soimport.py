fe4c50f393c86a9683938ae0b447ca2b5cc58d1a
Filip Haglund <drathier@users.noreply.github.com>
2016-11-02 19:41:25 +0100

    Update soimport.py

--- soimport.py
+++ soimport.py
@@ -1,3 +1,4 @@
+import html
 import re
 import sys
 from importlib._bootstrap import spec_from_loader
@@ -13,7 +14,9 @@ class StackOverflowImporter:
     @classmethod
     def find_spec(cls, fullname, path=None, target=None):
         print("find_spec", fullname, path, target)
-        return spec_from_loader(fullname, cls, origin='hell')
+        spec = spec_from_loader(fullname, cls, origin='hell')
+        spec.code = cls._fetch_code(spec.name.split(".", 1)[1].replace("_", " "))
+        return spec
 
     # def load_module(self, fullname):
     #    if fullname not in sys.modules:
@@ -21,35 +24,42 @@ class StackOverflowImporter:
     #    return sys.modules[fullname]
 
     @classmethod
-    def create_module(self, spec):
+    def create_module(cls, spec):
         """Create a built-in module"""
-        return spec
+        print("create_module", spec)
+        ns = {"__spec__": spec}
+        print("exec", spec.code)
+        exec(spec.code, ns)
+        return ns
 
     @classmethod
     def exec_module(cls, module=None):
         """Exec a built-in module"""
         print(module, type(module), dir(module))
-
+        return module
 
     @classmethod
     def get_code(cls, fullname):
+        print("get_code", fullname)
         return compile(cls._fetch_code(fullname), 'StackOverflow.com/' + fullname, 'exec')
 
     @classmethod
     def get_source(cls, fullname):
+        print("get_source", fullname)
         return cls.get_code(fullname)
 
     @classmethod
     def is_package(cls, fullname):
         print("is_pkg", fullname)
-        return True
+        return False
 
     ############################
 
     @classmethod
     def _fetch_code(cls, text):
         try:
-            ans = requests.get(cls.API_URL + "/search?order=desc&sort=votes&tagged=python&site=stackoverflow&intitle=" + text).json()
+            ans = requests.get(
+                cls.API_URL + "/search?order=desc&sort=votes&tagged=python&site=stackoverflow&intitle=" + text).json()
             print("ans", ans)
             q = requests.get(ans["items"][0]["link"])
             print("q", type(q.text), q.text)
@@ -65,14 +75,16 @@ class StackOverflowImporter:
         s = s[s.find('id="answer-'):]
         s = s[:s[100:].find('id="answer-')]
         print("s", s)
-        r = re.search(r"<pre[^>]*>[^<]*<code[^>]*>((?:\s|[^<]|<span[^>]*>[^<]+</span>)*)</code></pre>", s).group(1)  # heh
+        r = re.search(r"<pre[^>]*>[^<]*<code[^>]*>((?:\s|[^<]|<span[^>]*>[^<]+</span>)*)</code></pre>", s).group(
+            1)  # heh
         print("r", type(r), re.sub(r"<[^>]+>([^<]*)<[^>]*>", "\1", r))
 
-        return r
+        return html.unescape(r)
 
+
+sys.meta_path.append(StackOverflowImporter())
 
 if __name__ == "__main__":
-    sys.meta_path.append(StackOverflowImporter())
     # print(StackOverflowImporter("", "").exec_module("quick sort with python"))
     import sys
     import quick.sort
